e37bba9fa45045a76f77af3f8158de1d0236b9fb
paulizio <pauli.nordman@hotmail.com>
2021-09-23 20:04:35 +0300

    improved fetching and sorting of planets and residents

--- src/App.js
+++ src/App.js
@@ -1,86 +1,40 @@
-import React,{ useEffect,useState } from 'react';
-import axios from 'axios';
-import Planetsfilter from './components/Planetsfilter';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import Planetsfilter from './components/Planetsfilter'
+import './App.css'
 
-const App=() => {
-  const [planets,setPlanets]=useState([]);
-  const [residents,setResidents]=useState([]);
+const App = () => {
+  const [planets, setPlanets] = useState([])
+  const [residents, setResidents] = useState([])
 
-  //Get planets from all pages
+  // Get planets and residents from all pages
   useEffect(() => {
-    const first=axios.get('https://swapi.dev/api/planets');
-    const second=axios.get('http://swapi.dev/api/planets/?page=2');
-    const third=axios.get('http://swapi.dev/api/planets/?page=3');
-    const fourth=axios.get('http://swapi.dev/api/planets/?page=4');
-    const fifth= axios.get('http://swapi.dev/api/planets/?page=5');
-    const sixth=axios.get('http://swapi.dev/api/planets/?page=6');
-    axios.all([first,second,third,fourth,fifth,sixth]).then(
-      axios.spread(
-        (...responses) => {
-          const responseOne = responses[0];
-          const responseTwo = responses[1];
-          const responesThree = responses[2];
-          const responseFour=responses[3];
-          const responsesFive=responses[4];
-          const responsesSix=responses[5];
-          setPlanets(planets.concat(
-            responseOne.data.results,
-            responseTwo.data.results,
-            responesThree.data.results,
-            responseFour.data.results,
-            responsesFive.data.results,
-            responsesSix.data.results
-          ));
-        })
-    );},[]);
-
-  //Get residents from all pages
-  useEffect(() => {
-    const first=axios.get('https://swapi.dev/api/people');
-    const second=axios.get('http://swapi.dev/api/people/?page=2');
-    const third=axios.get('http://swapi.dev/api/people/?page=3');
-    const fourth=axios.get('http://swapi.dev/api/people/?page=4');
-    const fifth= axios.get('http://swapi.dev/api/people/?page=5');
-    const sixth=axios.get('http://swapi.dev/api/people/?page=6');
-    const seventh=axios.get('http://swapi.dev/api/people/?page=7');
-    const eigth=axios.get('http://swapi.dev/api/people/?page=8');
-    const ninth=axios.get('http://swapi.dev/api/people/?page=9');
-    axios.all([first,second,third,fourth,fifth,sixth,seventh,eigth,ninth]).then(
-      axios.spread(
-        (...responses) => {
-          const responseOne = responses[0];
-          const responseTwo = responses[1];
-          const responesThree = responses[2];
-          const responseFour=responses[3];
-          const responsesFive=responses[4];
-          const responsesSix=responses[5];
-          const responsesSeven=responses[6];
-          const responsesEight=responses[7];
-          const responsesNine=responses[8];
-          setResidents(planets.concat(
-            responseOne.data.results,
-            responseTwo.data.results,
-            responesThree.data.results,
-            responseFour.data.results,
-            responsesFive.data.results,
-            responsesSix.data.results,
-            responsesSeven.data.results,
-            responsesEight.data.results,
-            responsesNine.data.results
-          ));
-        })
-    );},[]);
+    const fetchStarWarsData = async (url, setData) => {
+      let data = []
+      let nextResponse
+      const response = await axios.get(url)
+      setData(prevData => [...prevData, ...response.data.results])
+      let nextUrl = response.data.next
+      while (nextUrl) {
+        nextResponse = await axios.get(nextUrl)
+        data = data.concat(nextResponse.data.results)
+        nextUrl = nextResponse.data.next
+      }
+      setData(prevData => [...prevData, ...data])
+    }
+    fetchStarWarsData('https://swapi.dev/api/planets', setPlanets)
+    fetchStarWarsData('https://swapi.dev/api/people', setResidents)
+  }, [])
 
   return (
-    <div className="center">
-      <img src={'https://fontmeme.com/permalink/201103/0869a6e4e4dd64aa577d10f506519065.png'} alt=""/>
+    <div className='center'>
+      <img src='https://fontmeme.com/permalink/201103/0869a6e4e4dd64aa577d10f506519065.png' alt='' />
       <div>
         <h1>Planets</h1>
-        <Planetsfilter planets={planets} residents={residents}/>
+        <Planetsfilter planets={planets} residents={residents} />
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
--- src/components/Planetsfilter.js
+++ src/components/Planetsfilter.js
@@ -1,17 +1,18 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
-import '../App.css';
-const Planetsfilter=({ planets,residents }) => {
-//Only show planets that have residents
-  let planetsWithResidents=planets.filter(p => p.residents.length!==0);
-  //Sort planets by diameter smallest to largest
-  let sortedByDiameter=planetsWithResidents.sort((a,b) => parseInt(isNaN(a.diameter)?0:a.diameter)-parseInt(isNaN(b.diameter)?0:b.diameter));
-  //Merge planets and residents together based on homeworld/url
-  let mergedArray = sortedByDiameter.map(planet => ({ ...planet, residents:residents.filter(res => res.homeworld===planet.url).map(res => res.name) }));
-
-
-  return(
-    <div className="tableFixHead">
+import React from 'react'
+import '../App.css'
+const Planetsfilter = ({ planets, residents }) => {
+  /*
+  Only show planets that have residents,
+  sort planets by diameter smallest to largest
+  and merge planets and residents based on homeworld/url
+  */
+  const planetsWithResidents = planets
+    .filter(p => p.residents.length !== 0)
+    .sort((a, b) => parseInt(isNaN(a.diameter) ? 0 : a.diameter) - parseInt(isNaN(b.diameter) ? 0 : b.diameter))
+    .map(planet => ({ ...planet, residents: residents.filter(res => res.homeworld === planet.url).map(res => res.name) }))
+  return (
+    <div className='tableFixHead'>
       <table>
         <thead>
           <tr>
@@ -21,11 +22,22 @@ const Planetsfilter=({ planets,residents }) => {
           </tr>
         </thead>
         <tbody>
-          {mergedArray.map(p => <tr key={p.name}><td >{p.name}</td><td>{p.diameter}</td><td>{p.residents.map(resident => <ul key={resident}>{resident}</ul>)}</td></tr>)}
+          {
+          planetsWithResidents
+            .map((p, i) =>
+              <tr key={i}>
+                <td>{p.name}</td>
+                <td>{p.diameter}</td>
+                <td>{p.residents.map((resident, i) =>
+                  <ul key={i}>{resident}</ul>
+                )}
+                </td>
+              </tr>
+            )
+}
         </tbody>
       </table>
     </div>
-  );
-};
-export default Planetsfilter;
-
+  )
+}
+export default Planetsfilter
